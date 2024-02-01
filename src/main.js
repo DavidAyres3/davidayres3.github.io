803b7e83e52872bb6549a4a3e22e721aa6f7873d
David Ayres <ayresdavid29@hotmail.com>
2024-01-31 22:17:13 -0300

    Update main.js
    Now users can delete tasks.

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ if(userData) {
     
     if(userObject && userObject.username){
         const username = userObject.username
-        title.innerHTML = `Lista de tarefas | ${username}`
+        title.innerHTML = `Olá, ${username}`
     }
     
 } else {
@@ -30,38 +30,72 @@ async function getUser() {
         });
 }
 
-function handleUserData() {
-    getUser()
-        .then(data => {
-            const user = data.id;
-            const userDataJSON = JSON.stringify(user)
-            if (localStorage.getItem('userId')) {
-                localStorage.removeItem('userId')
-            }
-            localStorage.setItem('userId', userDataJSON)
-            fetch(`https://dvd-deploy-cyclic.cyclic.app/tasks/${user}`)
-                .then(res => {
-                    if (res.status === 200)
-                        return res.json()
-                })
-                .then(data => {
-                    var taskList = document.getElementById('tarefas');
-                    data.forEach((task, indice) => {
-                        var div = document.createElement("div");
-                        var p = document.createElement("p");
-                        var strong = document.createElement("strong")
+async function handleUserData() {
+    try {
+        const data = await getUser();
+        const user = data.id;
+        const userDataJSON = JSON.stringify(user);
 
-                        p.textContent = task.description;
-                        strong.textContent = task.createdAt
-                        div.appendChild(p);
-                        div.appendChild(strong)
+        if (localStorage.getItem('userId')) {
+            localStorage.removeItem('userId');
+        }
 
-                        taskList.appendChild(div);
-                    });
-                })
-        })
+        localStorage.setItem('userId', userDataJSON);
+
+        const res = await fetch(`https://dvd-deploy-cyclic.cyclic.app/tasks/${user}`);
+
+        if (res.status === 200) {
+            const data = await res.json();
+
+            var taskList = document.getElementById('tarefas');
+            data.forEach(task => {
+                var div = document.createElement("div");
+                var p = document.createElement("p");
+                var strong = document.createElement("strong")
+                var img = document.createElement("img")
+
+                p.setAttribute("task-id", task.id)
+                p.textContent = task.description
+                strong.textContent = task.createdAt;
+                img.id = "del"
+                img.src = "/src/svg/icons8-lixeira.svg";
+
+                img.addEventListener("click", function() {
+                    handleImageClick(task.id);
+                });
+
+                div.appendChild(img);
+                div.appendChild(p);
+                div.appendChild(strong)
+
+                taskList.appendChild(div);
+            });
+        } else {
+            throw new Error('Erro na requisição para obter tarefas');
+        }
+    } catch (error) {
+        console.error('Erro:', error);
+    }
 }
+
 handleUserData()
+
+function handleImageClick(taskId) {
+    fetch(`https://dvd-deploy-cyclic.cyclic.app/task/delete/${taskId}`, {
+        method: 'DELETE'
+    })
+    .then(res => {
+        if (res.ok) {
+            console.log("Tarefa apagada com sucesso");
+            location.reload()
+        } else {
+            console.error("Erro ao apagar a tarefa:", res.statusText);
+        }
+    })
+    .catch(error => {
+        console.error("Erro na requisição:", error);
+    });
+}
 
 function newTask() {
     const id = localStorage.getItem('userId')
@@ -79,8 +113,6 @@ function newTask() {
         body: JSON.stringify(data)
     })
 }
-
-const taskList = document.getElementById('tarefas')
 
 
 enter.addEventListener('click', (e) => {
